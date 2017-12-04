d3efc9c1a14a795a44f33d48f0a8582bcc1dc2d1
zhenglsT007 <mr.zheng@dookay.com>
2017-12-04 11:53:28 +0800

    2017-12-4  14:35  时间转换

--- app/src/main/java/dookay/myapplication/TestActivity.java
+++ app/src/main/java/dookay/myapplication/TestActivity.java
@@ -1,17 +1,12 @@
 package dookay.myapplication;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
+import android.util.Log;
 
-import com.bumptech.glide.Glide;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import dookay.dklibrary.base.BaseActivity;
-import dookay.dklibrary.view.overlap_img.CircleImageView;
+import dookay.dklibrary.utils.ConstantIntUtils;
 import dookay.dklibrary.view.overlap_img.PileLayout;
 
 /**
@@ -35,16 +30,7 @@ public class TestActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        pileLayout = (PileLayout) findViewById(R.id.pile_layout);
-        List<String> strings = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                strings.add("http://img2.imgtn.bdimg.com/it/u=1939271907,257307689&fm=21&gp=0.jpg");
-            } else {
-                strings.add("http://img0.imgtn.bdimg.com/it/u=2263418180,3668836868&fm=206&gp=0.jpg");
-            }
-        }
-        pileLayout.setDataPraises(TestActivity.this, urls, pileLayout, R.mipmap.liblogo);
+
     }
 
     PileLayout pileLayout;
@@ -61,5 +47,34 @@ public class TestActivity extends BaseActivity {
     @Override
     protected void initData() {
 
+
+   /*     OkhttpInfoUtil  okhttpInfoUtil= new OkhttpInfoUtil(TestActivity.this) {
+            @Override
+            public void ShowResponse(String response, int okitem) {
+
+            }
+
+            @Override
+            public void ShowError(String response, int erritem) {
+
+            }
+        };*/
+        okhttpInfoUtil.getJson("http://ip-27-joyway-app.coralcodes.com/u/info?token=802db37eb25f40cd9db6dba6b2972cf17pM84c&client=android",
+                new HashMap<String, String>(), ConstantIntUtils.ZERO
+        );
     }
+
+    OkhttpInfoUtil okhttpInfoUtil = new OkhttpInfoUtil(TestActivity.this) {
+        @Override
+        protected void ShowInfoResponse(String response, int okitem) {
+
+            Log.e("info", "东方不败" + response);
+        }
+
+        @Override
+        protected void ShowInfoError(String response, int erritem) {
+            Log.e("info", "东方不败" + response);
+        }
+    };
+
 }
--- dookay/src/main/java/dookay/dklibrary/utils/OkhttpUtils.java
+++ dookay/src/main/java/dookay/dklibrary/utils/OkhttpUtils.java
@@ -1,6 +1,8 @@
 package dookay.dklibrary.utils;
 
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
@@ -8,6 +10,7 @@ import com.zhy.http.okhttp.callback.StringCallback;
 import java.io.File;
 import java.util.Map;
 
+import dookay.dklibrary.view.ToastView;
 import okhttp3.Call;
 
 /**
@@ -15,8 +18,7 @@ import okhttp3.Call;
  * Created by  on 2017/1/3.
  */
 public abstract class OkhttpUtils {
-
-    public void getJson(String url, final Map<String, String> map, final int i) {
+    public void getJson(final String url, final Map<String, String> map, final int i) {
         OkHttpUtils
                 .get()
                 .addHeader("Accept", "application/json")
@@ -25,7 +27,8 @@ public abstract class OkhttpUtils {
                 .execute(new StringCallback() {
                     @Override
                     public void onError(Call call, Exception e, int id) {
-
+                        Log.e("httpError", "Message:" + e.getMessage() + "  Url:" + url + "  Map:" + map.toString() + "  Code:" + i);
+                        ShowError(e.getMessage(), i);
                     }
                     @Override
                     public void onResponse(String response, int id) {
@@ -33,9 +36,7 @@ public abstract class OkhttpUtils {
                     }
                 });
     }
-
-    public void postJson(final String url, Map<String, String> map, final int i) {
-
+    public void postJson(final String url, final Map<String, String> map, final int i) {
         OkHttpUtils
                 .post()
                 .addHeader("Accept", "application/json")
@@ -44,6 +45,7 @@ public abstract class OkhttpUtils {
                 .execute(new StringCallback() {
                     @Override
                     public void onError(Call call, Exception e, int id) {
+                        Log.e("httpError", "Message:" + e.getMessage() + "  Url:" + url + "  Map:" + map.toString() + "  Code:" + i);
                         ShowError(e.getMessage(), i);
                     }
 
@@ -61,7 +63,7 @@ public abstract class OkhttpUtils {
      * @param file
      * @param i
      */
-    public void postFile(String url, File file, final int i) {
+    public void postFile(final String url, final File file, final int i) {
         OkHttpUtils.post()
                 .addHeader("Accept", "application/json")
                 .addFile("file", file.getName(), file)
@@ -70,6 +72,7 @@ public abstract class OkhttpUtils {
                 .execute(new StringCallback() {
                     @Override
                     public void onError(Call call, Exception e, int id) {
+                        Log.e("httpError", "Message:" + e.getMessage() + "  Url:" + file + "  Code:" + i);
                         ShowError(e.getMessage(), i);
                     }
 
@@ -79,7 +82,7 @@ public abstract class OkhttpUtils {
                     }
                 });
     }
-    
+
     public void getJsonResutFive(final String url, final Map<String, String> map, final int i) {
         OkHttpUtils
                 .get()
@@ -89,6 +92,7 @@ public abstract class OkhttpUtils {
                 .execute(new StringCallback() {
                     @Override
                     public void onError(Call call, Exception e, int id) {
+                        Log.e("httpError", "Message:" + e.getMessage() + "  Url:" + url + "  Map:" + map.toString() + "  Code:" + i);
                         ShowError(e.getMessage(), i);
                     }
 
